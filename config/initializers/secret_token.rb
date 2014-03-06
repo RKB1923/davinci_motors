6e8678931c5ac397966978b3cc66ef3b6bea76de
Rodney Bautista <Rodney.K.Bautista@gmail.com>
2014-03-05 20:04:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DavinciMotors::Application.config.secret_key_base = '01536620c0e6b055b81eaf3fdaa7c00d65f2261a675d4083b072a1687c285a835978db92b15b6d9e02540f3699a92458eed8d17efe5364b165229c1fe441ae38'
